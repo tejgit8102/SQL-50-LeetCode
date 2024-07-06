2ce460ad531ee08d747ea6c579f2bd2243d52ed4
tejgit8102 <166322007+tejgit8102@users.noreply.github.com>
2024-07-06 21:22:44 +0530

    Create 38_626_Exchange_Seats.sql
    Leetcode-SQL-50-Problem-38

---
+++ 38_626_Exchange_Seats.sql
@@ -0,0 +1,9 @@
+SELECT
+    (CASE 
+        WHEN id % 2 = 0 THEN id - 1
+        WHEN id = (SELECT COUNT(*) FROM Seat) THEN id
+        ELSE id + 1
+    END) AS id,
+    student
+FROM Seat
+ORDER BY id;
